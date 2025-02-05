dead764f22b372b826c9437e987e96c993f115a8
Hemalatha0510 <150413251+Hemalatha0510@users.noreply.github.com>
2025-02-05 16:04:52 +0530

    Operators

--- Basic.c
+++ Basic.c
@@ -14,3 +14,16 @@ sum = num1 + num2;
 printf("Sum = %d\n", sum);
 return 0;
 }
+
+#include <stdio.h>
+int main() {
+int a, b;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+printf("Addition: %d\n", a + b);
+printf("Subtraction: %d\n", a - b);
+printf("Multiplication: %d\n", a * b);
+printf("Division: %.2f\n", (float)a / b);
+printf("Modulus: %d\n", a % b);
+return 0;
+}
