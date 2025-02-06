e41601ceb2d5293b3aae20feeb0bfa6739db4081
Hemalatha0510 <150413251+Hemalatha0510@users.noreply.github.com>
2025-02-06 11:58:55 +0530

    Multiplying two float numbers

--- Practice_programs.c
+++ Practice_programs.c
@@ -20,3 +20,12 @@ int main() {
     printf("%s \n" , name);
     return 0;
 }
+
+#include <stdio.h>
+int main() {
+    float a, b;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+    printf("Product: %.2f\n", a * b);
+    return 0;
+}
