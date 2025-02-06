d0e0cfea971dcf0983690af771ad99ee0cebd687
Hemalatha0510 <150413251+Hemalatha0510@users.noreply.github.com>
2025-02-06 14:15:31 +0530

    2

--- 06.02.25.c
+++ 06.02.25.c
@@ -34,3 +34,24 @@ int main()
     }
     return 0;
 } 
+
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num % 3 == 0 && num % 5 == 0) {
+        printf("The number is divisible by both 3 and 5 \n", num);
+    } 
+    else if(num % 3 == 0) {
+        printf("The number is divisible by 3 \n", num);
+    }
+    else if(num % 5 == 0) {
+        printf("The number is divisible by 5 \n", num);
+    } 
+    else {
+        printf("The number is not divisible by either 3 or 5 \n", num);
+    }
+    return 0;
+}
